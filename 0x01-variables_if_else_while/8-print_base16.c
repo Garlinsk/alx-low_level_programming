289e1d35d65e6ae0a7e272b7946c66e8d028be1c
Garlinsk <franklinwanami@gmail.com>
2022-07-02 06:43:57 -0700

    print_base16.c

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -9,15 +9,14 @@ int main(void)
 {
 	int i, y;
 	for (i = 0; i < 10; i++)
- 	{
-		putchar (i + '0');
-	}
-	   for (y = 'a'; y <= 'f'; y++)
-	   {
-		   putchar (y);
-	   }
-
-	   putchar('\n');
-	   return (0);
+	    {
+		     putchar (i + '0');
+	    }
+	           for (y = 'a'; y <= 'f'; y++)
+		       {
+			       putchar (y);
+		       }
+		    putchar('\n');
+		    return (0);
 }
 
