8588ec4079bfaacd337004f06f079252349e9ee1
Garlinsk <franklinwanami@gmail.com>
2022-07-05 14:39:09 -0700

    times_table.c

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -1,5 +1,6 @@
 #include "main.h"
 /**
+ * main
  *times_table -prints tables
  *Return: Always 0.
  */
@@ -8,6 +9,7 @@ void times_table(void)
 	int a;
 	int b;
 	int c;
+
 	for (a = 0; a <= 9; a++)
 	{
 		for (b = 0; b <= 9; b++)
